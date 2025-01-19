031079ff1fd33d0b594dec27146330b00d3cb013
aiki.w1817137 <aiki.w1817137@gmail.com>
2025-01-19 21:56:04 +0900

    feature:button.tsxを追加

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,9 @@
 export default function Button() {
   return (
     <div>
-      <button>Test Button</button>
+      <button className="px-4 py-2 rounded bg-blue-500 hover:bg-blue-700 text-white font-bold">
+        Test Button
+      </button>
     </div>
   );
 }
